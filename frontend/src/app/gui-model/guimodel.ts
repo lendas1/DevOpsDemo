3099cdc8ea14dd50b61f16a803436cb8923bc325
lendas1 <0815konto123@gmail.com>
2024-05-20 16:46:10 +0200

    2.0

--- frontend/src/app/gui-model/guimodel.ts
+++ frontend/src/app/gui-model/guimodel.ts
@@ -107,6 +107,15 @@ export class GuiModel {
                             "width": 2,
                             "newRow": true,
                         },
+                        {
+                            "type": "button",
+                            "name": { default: "Einkaufsliste" },
+                            "icon": "fa-file-alt",
+                            "color": "wet-asphalt",
+                            "page": "toDoPage",
+                            "width": 2,
+                            "newRow": true,
+                        },
                     ]
                 },
                 {
